0672c04968b6251e957d82de9c56e19698f0ee7e
rashadmohamedr <rashadmohamed2019@icloud.com>
2025-11-16 21:35:54 +0200

    Refactor File model to include owner relationship and update table name

--- app/models/file.py
+++ app/models/file.py
@@ -1,14 +1,17 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from app.db.database import Base
 from datetime import datetime
 
 # Task table
 class File(Base):
-    __tablename__ = "Tasks"
+    __tablename__ = "Files"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    ownerID = Column(Integer, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     content_type = Column(String,nullable=True)
     path  = Column(String, unique=True, nullable=False)
     uploaded_at = Column(DateTime, default=datetime.utcnow)
+    
+    owner = relationship("User", back_populates="files")
